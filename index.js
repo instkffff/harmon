b2ed149c87e365d7bde36e7ff57af69303451181
Grant Levene <glevene@arbor.net>
2018-10-06 19:38:59 -0400

    Make isHtml & isGzipped methods on ServerRequest
    Idk this is probably bad but the tests pass and I feel that the new
    methods have names unique enough to not cause conflicts sooooo...

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 var trumpet = require('trumpet');
 var zlib = require('zlib');
+var { ServerResponse } = require('http');
+
+
+/* Determine if this is a request for HTML and cache the result */
+ServerResponse.prototype.__harmon_isHtml = function () {
+  if (this.__harmon__isHtml === undefined) {
+    var contentType = this.getHeader('content-type') || '';
+    this.__harmon__isHtml = contentType.indexOf('text/html') === 0;
+  }
+
+  return this.__harmon__isHtml;
+}
+
+/* Determine if this is a request for compressed content and cache the result */
+ServerResponse.prototype.__harmon_isGzipped = function () {
+  if (this.__harmon__isGzipped === undefined) {
+    var encoding = this.getHeader('content-encoding') || '';
+    this.__harmon__isGzipped = encoding.toLowerCase() === 'gzip' && this.__harmon_isHtml();
+  }
+
+  return this.__harmon__isGzipped;
+}
+
 
 module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
   var _reqSelectors = reqSelectors || [];
   var _resSelectors = resSelectors || [];
   var _htmlOnly     = (typeof htmlOnly == 'undefined') ? false : htmlOnly;
-
-  function prepareRequestSelectors(req, res) {
-    var tr = trumpet();
-
-    prepareSelectors(tr, _reqSelectors, req, res);
-
-    req.on('data', function(data) {
-      tr.write(data);
-    });
-  }
 
   function prepareResponseSelectors(req, res) {
     var tr          = trumpet();
@@ -25,40 +38,23 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
 
     prepareSelectors(tr, _resSelectors, req, res);
 
-    res.isHtml = function () {
-      if (res._isHtml === undefined) {
-        var contentType = res.getHeader('content-type') || '';
-        res._isHtml = contentType.indexOf('text/html') === 0;
-      }
-
-      return res._isHtml;
-    }
-
-    res.isGzipped = function () {
-      if (res._isGzipped === undefined) {
-        var encoding = res.getHeader('content-encoding') || '';
-        res._isGzipped = encoding.toLowerCase() === 'gzip' && res.isHtml();
-      }
-
-      return res._isGzipped;
-    }
-
     res.writeHead = function () {
-      var headers = (arguments.length > 2) ? arguments[2] : arguments[1]; // writeHead supports (statusCode, headers) as well as (statusCode, statusMessage, headers)
+      // writeHead supports (statusCode, headers) as well as (statusCode, statusMessage, headers)
+      var headers = (arguments.length > 2) ? arguments[2] : arguments[1];
       headers = headers || {};
 
       /* Sniff out the content-type header.
        * If the response is HTML, we're safe to modify it.
        */
-      if (!_htmlOnly && res.isHtml()) {
+      if (!_htmlOnly && res.__harmon_isHtml()) {
         res.removeHeader('Content-Length');
         delete headers['content-length'];
       }
 
       /* Sniff out the content-encoding header.
-       * If the response is Gziped, we're have to gunzip content before and ungzip content after.
+       * If the response is Gziped, we have to gunzip content before and ungzip after.
        */
-      if (res.isGzipped()) {
+      if (res.__harmon_isGzipped()) {
         res.removeHeader('Content-Encoding');
         delete headers['content-encoding'];
       }
@@ -68,8 +64,8 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
 
     res.write = function (data, encoding) {
       // Only run data through trumpet if we have HTML
-      if (res.isHtml()) {
-        if (res.isGzipped()) {
+      if (res.__harmon_isHtml()) {
+        if (res.__harmon_isGzipped()) {
           gunzip.write(data);
         } else {
           tr.write(data, encoding);
@@ -79,28 +75,28 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
       }
     };
 
-    tr.on('data', function (buf) {
-      _write.call(res, buf);
-    });
-
-    gunzip.on('data', function (buf) {
-      tr.write(buf);
-    });
-
     res.end = function (data, encoding) {
-      if (res.isGzipped()) {
+      if (res.__harmon_isGzipped()) {
         gunzip.end(data);
       } else {
         tr.end(data, encoding);
       }
     };
 
-    gunzip.on('end', function (data) {
-      tr.end(data);
+    tr.on('data', function (buf) {
+      _write.call(res, buf);
     });
 
     tr.on('end', function () {
       _end.call(res);
+    });
+
+    gunzip.on('data', function (buf) {
+      tr.write(buf);
+    });
+
+    gunzip.on('end', function (data) {
+      tr.end(data);
     });
   }
 
